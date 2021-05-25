0e7cc6f840b31742897154be1d79db9bac7b1361
Northy05 <83496104+Northy05@users.noreply.github.com>
2021-05-25 20:18:49 +0200

    Update hh.py

--- hh.py
+++ hh.py
@@ -4,7 +4,7 @@ import requests
 if __name__ == '__main__':
 
     hosts_file = open('HOSTS','r')
-    valid = "Cloudflare"
+    valid = "Cloudfront"
 
     user_agent = {
 	"User-Agent" : "Browser: Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36"
